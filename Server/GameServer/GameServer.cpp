765e3a3f855060af232bce208ec4635e13cb9394
규규\Lenovo <kyuhi>
2023-07-26 16:59:05 +0900

    가시성과 코드재배치로 인한 현상

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -11,33 +11,48 @@
 #include "UserManager.h"
 
 
-int32 iBufferArray[10000][10000];
+// 가시성, 코드 재배치
+int32 x = 0;
+int32 y = 0;
+int32 r1 = 0;
+int32 r2 = 0;
+
+volatile bool bReady;
+
+
+void Thread_1() {
+	while (!bReady)
+		;
+	y = 1; // Store Y
+	r1 = x; //LoadX
+}
+void Thread_2() {
+	while (!bReady)
+		;
+	x = 1;
+	r2 = y;
+}
 
 int main()
 {
-	memset(iBufferArray, 0, sizeof(iBufferArray));
-	{
-		uint64 start = GetTickCount64();
-		int64 sum = 0;
-		for (int32 i = 0; i < 10000; i++) {
-			for (int32 j = 0; j < 10000; j++) {
-				sum += iBufferArray[i][j];
-			}
-		}
-		uint64 end = GetTickCount64();
-		cout << end - start << endl;
+	int32 count = 0;
+	while (1) {
+		bReady = false;
+		count++;
+
+		x = y = r1 = r2 = 0;
+		thread t1(Thread_1);
+		thread t2(Thread_2);
+
+		bReady = true;
+
+		t1.join();
+		t2.join();
+
+		if (r1 == 0 && r2 == 0)
+			break;
 	}
 
-	{
-		uint64 start = GetTickCount64();
-		int64 sum = 0;
-		for (int32 i = 0; i < 10000; i++) {
-			for (int32 j = 0; j < 10000; j++) {
-				sum += iBufferArray[j][i];
-			}
-		}
-		uint64 end = GetTickCount64();
-		cout << end - start << endl;
-	}
+	cout << count << " 번 만에 빠져나옴!!!!" << endl;
 }
 
