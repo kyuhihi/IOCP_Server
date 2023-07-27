8b524a35ae40a7176429406ba9af6184e1b223e6
규규\Lenovo <kyuhi>
2023-07-27 15:41:32 +0900

    TLS를 이용하면 쓰레드 내부에서 사용하는 로컬변수로 사용할수있다.

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -9,77 +9,31 @@
 #include <future>
 #include "AccountManager.h"
 #include "UserManager.h"
+// TLS
+thread_local int32 LThreadId = 0;
 
-//atomic<bool> bFlag;
-
-atomic<bool> ready;
-int32 value;
-
-void Producer() {
-
-	value = 10;
-	ready.store(true, memory_order::memory_order_seq_cst);
-}
-
-void Consumer() {
-	while (ready.load(memory_order::memory_order_seq_cst) == false) {
-		;
+void ThreadMain(int32 threadId) {
+	LThreadId = threadId;
+	
+	while (true) {
+		cout << "Hi I am Thread" << LThreadId << endl;
+		this_thread::sleep_for(1s);
 	}
-	cout << value << endl;;
 }
 
 int main()
 {
-#pragma region 기본적인 atomic 사용 함수 + 방법.
-	//bFlag = false;
+	vector<thread> threads;
+	for (int32 i = 0; i < 10; i++)
+	{
+		int32 threadId = i + 1;
+		threads.push_back(thread(ThreadMain, threadId));
+	}
 
-	////bFlag.store(true,memory_order::memory_order_seq_cst);
-
-	////bool val = bFlag.load(memory_order::memory_order_seq_cst);
-	////cout << val;
-	//{
-	//	bool bPrev = bFlag.exchange(true);// exchange는 인자로 채우고싶은 값을받고, return값으로 기존값을 리턴.
-
-	//	//bool bPrev = bFlag.load();// 다른쓰레드가 flab값을 바꿨다면 유효하지 않은 prev값이 될수있음.
-	//	//bFlag.store(false);
-	//	
-	//}
-	////CAS Compare and Swap(조건부로 수정하겠다.)
-	//{
-	//	bool expected = false;
-	//	bool desired = true;
-	//	bFlag.compare_exchange_strong(expected, desired);
-	//	// 위코드의 의사코드
-	//	if (bFlag == expected) {
-	//		bFlag = desired;
-	//		return true;
-	//	}
-	//	else {
-	//		expected = bFlag;
-	//		return false;
-	//	}
-	//}
-
-#pragma endregion
-
-	ready = false;
-	value = 0;
-	thread t1(Producer);
-	thread t2(Consumer);
-	t1.join();
-	t2.join();
-
-
-	// memory order 메모리 정책
-	// 1) Sequentially Consistant 일관성있다. (memory_order::memory_order_seq_cst)
-	// 2) Acquire-Release(acquire, release)
-	// 3) Relaxed (relaxed)
-
-
-	// 1번은 엄격(컴파일러입장에서 최적화 여지가 적다= 직관적)=> 가시성 바로해결.
-	// 2번은 
-	// Relaxed 자유롭다 최적화 여지가 많다..
-
+	for (auto& iter : threads)
+	{
+		iter.join();
+	}
 
 }
 
