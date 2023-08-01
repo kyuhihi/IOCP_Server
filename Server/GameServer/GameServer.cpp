076ac4c93ec07c967e324a312f9ab6ee18b66120
규규\Lenovo <kyuhi>
2023-08-01 17:48:27 +0900

    연습문제: 소수판별법! 멀티쓰레딩으로 처리

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -9,34 +9,69 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include "PlayerManager.h"
-#include "AccountManager.h"
+// 소수 구하기.
+const int32 MAX_NUMBER = 100'0000;
+const int16 iThreadMaxNum = 5;
 
+
+list<int32> vResult;
+mutex m;
+
+void isPrime(int16 iThreadNum)
+{
+	int32 iMakeMaxNum = MAX_NUMBER / iThreadMaxNum;
+	for (int32 i = iThreadNum * iMakeMaxNum; i < iMakeMaxNum * (iThreadNum + 1) ; ++i)
+	{
+		if (i <= 1)//1보다 작으면 의미없음
+			continue;
+
+		if (i % 2 == 0)// 2로 나눠지면 의미없음
+			continue;
+
+		bool bSoSu = true;
+		for (int32 j = 3; j <= sqrt(i); j += 2)
+		{// 루트씌워서 더 작은값으로 나눠서 나눠떨어지면 소수아님
+			if (i % j == 0) {
+				bSoSu = false;
+				break;
+			}
+		}
+
+		if (bSoSu) {// 성공하면 vector에 담자
+			m.lock();
+			vResult.push_back(i);
+			m.unlock();
+		}
+	}
+	cout << iThreadNum << "번 쓰레드 성공!" << endl;
+}
 
 int main()
 {
-	GThreadManager->Launch([=] 
-		{
-			while (true)
-			{
-				cout << "PlayerThenAccount" << endl;
-				GPlayerMananger.PlayerThenAccount();
+	vector<thread> tVec;
+	for (int16 iThreadNum = 0; iThreadNum < iThreadMaxNum; ++iThreadNum)
+	{
+		tVec.push_back(thread(isPrime, iThreadNum));
+	}
 
-				this_thread::sleep_for(100ms);
-			}
-		});
+	for (auto& iter : tVec) {
+		if(iter.joinable())
+			iter.join();
+	}
 
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "AccountThenPlayer" << endl;
-				GAccountMananger.AccountThenPlayer();
-
-				this_thread::sleep_for(100ms);
-			}
-		});
 
 	GThreadManager->Join();
+
+	
+	vResult.push_back(2);
+
+	vResult.sort();
+	
+	for (auto& iter : vResult) 
+		cout << iter << endl;
+	
+	cout << "사이즈는 " << vResult.size();
+
+
 }
 
