356548f8f0332db5454b5a2b7a5e17f6b719a1b1
규규\Lenovo <kyuhi>
2023-08-05 00:21:35 +0900

    락프리로 구현한 메모리풀링 Node단위가 아님.  ABA 문제를 핵결하기위해 카운트를 두었다

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -11,42 +11,58 @@
 #include "RefCounting.h"
 #include "Memory.h"
 #include "Allocator.h"
+#include "LockFreeStack.h"
 
-class Knight
+DECLSPEC_ALIGN(16)
+class Data
 {
 public:
-	Knight()
-	{
-		cout << "Knight생성" << endl;
-	}
-	Knight(int32 hp): _hp(hp)
-	{
-		cout << "Knight생성 인자있음" << endl;
-	}
-	~Knight()
-	{
-		cout << "Knight 소멸" << endl;
-	}
-public:
-	int32 _hp = 30;
-	int32 _mp = 10;
+	SListEntry _entry;
+	int64 _rand = rand() % 100;
+
+
 };
 
+SListHeader* GHeader;
 
 int main()
 {
-	for (int32 i = 0; i < 5; ++i)
+	GHeader = new SListHeader;
+	ASSERT_CRASH(((uint64)GHeader % 16) == 0);
+	InitializeHead(GHeader);
+
+	for (int32 i = 0; i < 3; i++)
 	{
 		GThreadManager->Launch([]()
 			{
 				while (true)
 				{
-					Vector<Knight> v(10);
+					Data* data = new Data();
+					ASSERT_CRASH(((uint64)data% 16) == 0);
 
-					Map<int32, Knight> m;
-					m[100] = Knight();
-
+					PushEntrySList(GHeader, (SListEntry*)data);
 					this_thread::sleep_for(10ms);
+				}
+			});
+	}
+	for (int32 i = 0; i < 2; i++)
+	{
+		GThreadManager->Launch([]()
+			{
+				while (true)
+				{
+					Data* pop = nullptr;
+					pop = (Data*)PopEntrySList(GHeader);
+
+					if (pop)
+					{
+						cout << pop->_rand << endl;
+						delete pop;
+					}
+					else
+					{
+						cout << "NONE" << endl;
+					}
 				}
 			});
 	}
