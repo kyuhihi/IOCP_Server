6206672a45a1f2a3b9261ac21dd0cf80a2e05b3a
규규\Lenovo <kyuhi>
2023-08-08 18:13:11 +0900

    1. send recv 함수는 blocking함수로 실행이되지 않을시 잠시 잠잔다. 2. send 함수를 다중으로 호출함에도 recv시 데이터가 하나로 인지된다.

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -11,17 +11,13 @@
 
 int main()
 {
-	// 윈socket 초기화
 	WSAData wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 	{
 		return 0;
 	}
 
-	// af: Address family( AF_INET - ipv4, AF_INET6 - ipv6)
-	// type: TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
-	// protocol : 0 자동선택
-	// return : socket desc
+
 	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == INVALID_SOCKET)
 	{
@@ -30,19 +26,13 @@ int main()
 		return 0;
 	}
 
-	SOCKADDR_IN serverAddr; //IPv4 기준 이거 사용.
+	SOCKADDR_IN serverAddr; 
 	::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 
-	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1");// 구식 방법임
 	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
 
-	//	host to network short
-	//	Little- Endian vs Big-Endian
-	// ex) 0x12345678 4byte 정수
-	// low [0x78][0x56][0x34][0x12] high < little
-	// low [0x12][0x34][0x56][0x78] high > big = network
-	//네트워크에서 공식적으로 사용하는 endian방식은 big이다.
+
 	serverAddr.sin_port = ::htons(7777);
 
 
@@ -59,6 +49,36 @@ int main()
 	cout << "Connected To Server!!" << endl;
 	while (true)
 	{
+		char sendBuffer[100] = "Hello World!";
+		//TODO
+
+		for (int i = 0; i < 10; ++i)
+		{
+			int32 resultCode = send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+			if (resultCode == SOCKET_ERROR)
+			{
+				int32 errCode = ::WSAGetLastError();
+				cout << "SEND ErrorCode :" << errCode << endl;
+				return 0;
+			}
+		}
+		
+
+		cout << "Send Data! Len " << sizeof(sendBuffer) << endl;
+
+		/*char recvBuffer[1000];
+		int32 recvLen = recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+		if (recvLen <= 0)
+		{
+			int32 errCode = WSAGetLastError();
+			cout << "RECV ErrorCode :" << errCode << endl;
+			return 0;
+		}
+		cout << "Recv Data! Data" << recvBuffer << endl;
+		cout << "Recv Data! Len" << recvLen << endl;*/
+
+
+
 		this_thread::sleep_for(1s);
 	}
 
--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -21,10 +21,7 @@ int main()
 		return 0;
 	}
 
-	// af: Address family( AF_INET - ipv4, AF_INET6 - ipv6)
-	// type: TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
-	// protocol : 0 자동선택
-	// return : socket desc
+	
 	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (listenSocket == INVALID_SOCKET)
 	{
@@ -35,11 +32,11 @@ int main()
 
 
 
-	SOCKADDR_IN serverAddr; //IPv4 기준 이거 사용.
+	SOCKADDR_IN serverAddr; 
 	::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 
-	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);// 알아서 골라주
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
 	serverAddr.sin_port = ::htons(7777);
 
 	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
@@ -59,7 +56,7 @@ int main()
 	//---------------------------------
 	while (true)
 	{
-		SOCKADDR_IN clientAddr; //IPv4 기준 이거 사용.
+		SOCKADDR_IN clientAddr; 
 		::memset(&clientAddr, 0, sizeof(clientAddr));
 		int32 addrLen = sizeof(clientAddr);
 
@@ -76,6 +73,37 @@ int main()
 		char ipAddress[16];
 		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
 		cout << "client Connected!!! IP" << ipAddress<<endl;
+	
+		while (true)
+		{
+			char recvBuffer[1000];
+
+			this_thread::sleep_for(1s);
+
+			int32 recvLen = recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen <= 0)
+			{
+				int32 errCode = WSAGetLastError();
+				cout << "RECV ErrorCode :" << errCode << endl;
+				return 0;
+			}
+			cout << "Recv Data! Data " << recvBuffer << endl;
+			cout << "Recv Data! Len " << recvLen << endl;
+			
+
+
+			/*int32 resultCode = send(clientSocket, recvBuffer, recvLen, 0);
+
+			if (resultCode == SOCKET_ERROR)
+			{
+				int32 errCode = ::WSAGetLastError();
+				cout << "SEND ErrorCode :" << errCode << endl;
+				return 0;
+			}*/
+
+		
+		}
+	
 	}
 
 	//---------------------------------
