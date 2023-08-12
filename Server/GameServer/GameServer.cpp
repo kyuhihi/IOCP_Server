315cce5836a37ce1c6a97790a73b3e332329bf52
규규\Lenovo <kyuhi>
2023-08-12 22:39:46 +0900

    udp 소캣 프로그래밍 어떤이유에서인지 10054 소켓에러가 뜨며 recv가 안된다

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,113 +1,82 @@
 ﻿#include "pch.h"
+#include <iostream>
 #include "CorePch.h"
-
-#include <thread>
 #include <atomic>
 #include <mutex>
 #include <windows.h>
 #include <future>
 #include "ThreadManager.h"
 
-#include <WinSock2.h>
-#include <MSWSock.h>
+#include <winsock2.h>
+#include <mswsock.h>
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
+
+void HandleError(const char* cause)
+{
+	int32 errCode = ::WSAGetLastError();
+	cout << cause << " ErrorCode : " << errCode << endl;
+}
 
 int main()
 {
 	WSAData wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	SOCKET serverSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	if (serverSocket == INVALID_SOCKET)
 	{
+		HandleError("Socket");
 		return 0;
 	}
 
-	
-	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
-	if (listenSocket == INVALID_SOCKET)
-	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "Socket ErrorCode :" << errCode << endl;
-		return 0;
-	}
-
-
-
-	SOCKADDR_IN serverAddr; 
+	SOCKADDR_IN serverAddr;
 	::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
-
 	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
 	serverAddr.sin_port = ::htons(7777);
 
-	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	if (::bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
 	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "BIND ErrorCode :" << errCode << endl;
+		HandleError("Bind");
 		return 0;
 	}
 
-	if (::listen(listenSocket, 10))
-	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "LISTEN ErrorCode :" << errCode << endl;
-		return 0;
-	}
-
-	//---------------------------------
 	while (true)
 	{
-		SOCKADDR_IN clientAddr; 
+		SOCKADDR_IN clientAddr;
 		::memset(&clientAddr, 0, sizeof(clientAddr));
 		int32 addrLen = sizeof(clientAddr);
 
-		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		this_thread::sleep_for(1s);
 
-		if (clientSocket == INVALID_SOCKET)
+		char recvBuffer[1000];
+
+		int32 recvLen = ::recvfrom(serverSocket, recvBuffer, sizeof(recvBuffer), 0,
+			(SOCKADDR*)&clientAddr, &addrLen);
+
+		if (recvLen <= 0)
 		{
-			int32 errCode = ::WSAGetLastError();
-			cout << "ACCEPT ErrorCode :" << errCode << endl;
+			HandleError("RecvFrom");
 			return 0;
 		}
 
-		// 손님입장!
-		char ipAddress[16];
-		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
-		cout << "client Connected!!! IP" << ipAddress<<endl;
-	
-		while (true)
+		cout << "Recv Data! Data = " << recvBuffer << endl;
+		cout << "Recv Data! Len = " << recvLen << endl;
+
+		int32 errorCode = ::sendto(serverSocket, recvBuffer, recvLen, 0,
+			(SOCKADDR*)&clientAddr, sizeof(clientAddr));
+
+		if (errorCode == SOCKET_ERROR)
 		{
-			char recvBuffer[1000];
-
-			this_thread::sleep_for(1s);
-
-			int32 recvLen = recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-			if (recvLen <= 0)
-			{
-				int32 errCode = WSAGetLastError();
-				cout << "RECV ErrorCode :" << errCode << endl;
-				return 0;
-			}
-			cout << "Recv Data! Data " << recvBuffer << endl;
-			cout << "Recv Data! Len " << recvLen << endl;
-			
-
-
-			/*int32 resultCode = send(clientSocket, recvBuffer, recvLen, 0);
-
-			if (resultCode == SOCKET_ERROR)
-			{
-				int32 errCode = ::WSAGetLastError();
-				cout << "SEND ErrorCode :" << errCode << endl;
-				return 0;
-			}*/
-
-		
+			HandleError("SendTo");
+			return 0;
 		}
-	
+
+		cout << "Send Data! Len = " << recvLen << endl;
 	}
 
-	//---------------------------------
-
+	// 윈속 종료
 	::WSACleanup();
 }
-
